59ffbc792f51037d2bf07fd0f189717507a39a09
Shihabuddin9 <shihab.bd.pk@gmail.com>
2024-05-08 09:51:45 +0600

    Navbar add

--- src/component/Routes/Routes.jsx
+++ src/component/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from '../Layout/Root'
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
 
 
 const Router = createBrowserRouter([
@@ -9,7 +10,10 @@ const Router = createBrowserRouter([
         element: <Root></Root>,
         errorElement: <ErrorPage />,
         children: [
-            {},
+            {
+                path: '/',
+                element: <Home></Home>
+            },
         ],
     },
 ])
--- src/component/Shared/Navbar/Navbar.jsx
+++ src/component/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,14 @@
+import { Link, NavLink } from "react-router-dom";
+import { FaCartPlus } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+
 const Navbar = () => {
+    const Links = <>
+        <li> <NavLink to='/'>Home</NavLink></li>
+        <li><NavLink className='mx-5' to='/menu'>Menu</NavLink></li>
+        <li> <NavLink to='/mobileApp'>Mobile app</NavLink></li>
+        <li><NavLink className='ml-5' to='/ContactUs'>Contact us</NavLink></li>
+    </>
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -7,36 +17,24 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {Links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl font-serif">FirstBuy</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {Links}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <div className=" md:text-2xl">
+                    <CiSearch />
+                </div>
+                <div className="mx-5 md:text-2xl">
+                    <FaCartPlus />
+                </div>
+                <Link to='/signIn' className="btn">Sign in</Link>
             </div>
         </div>
     );
