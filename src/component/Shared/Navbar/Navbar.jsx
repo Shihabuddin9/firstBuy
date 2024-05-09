0e196003dbffae01cea162e26032e3d75666704f
Shihabuddin9 <shihab.bd.pk@gmail.com>
2024-05-10 00:16:03 +0600

    Navbar update

--- src/component/Shared/Navbar/Navbar.jsx
+++ src/component/Shared/Navbar/Navbar.jsx
@@ -1,25 +1,44 @@
 import { Link, NavLink } from "react-router-dom";
 import { FaCartPlus } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
     const Links = <>
         <li> <NavLink to='/'>Home</NavLink></li>
         <li><NavLink className='md:mx-5' to='/menu'>Menu</NavLink></li>
         <li> <NavLink to='/mobileApp'>Mobile app</NavLink></li>
         <li><NavLink className='md:ml-5' to='/ContactUs'>Contact us</NavLink></li>
     </>
+
+    const handleClose = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
+
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <div onClick={handleClose} tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        {Links}
-                    </ul>
+
+                    <div className="relative">
+                        <div className={`fixed left-0 top-0 z-30 flex h-screen w-4/5 max-w-full bg-white shadow-lg ${isOpen ? 'hidden translate-x-full' : "block translate-x-0"}`}>
+
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-10 ml-5 z-[1] p-2">
+                                {Links}
+                            </ul>
+
+                            <button onClick={handleClose} className="top-5 text-3xl absolute right-10">X</button>
+                        </div>
+                    </div>
+
+
                 </div>
+
                 <a className="text-xl font-serif"><span className="text-3xl text-red-700">F</span>irstBuy</a>
             </div>
             <div className="navbar-center hidden lg:flex">
